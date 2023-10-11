7e5cb9136aca94ffd2eb876d04ae213d4308f5ae
Eduard-Epurica <epurica.eduard@gmail.com>
2023-10-11 17:17:38 +0300

    Initial Commit

---
+++ src/main/java/com/eduard/RESTCourse/RestCourseApplication.java
@@ -0,0 +1,13 @@
+package com.eduard.RESTCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCourseApplication.class, args);
+	}
+
+}
